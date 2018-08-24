2441f7259413a9fc0de44ec146b646e25d7e88a0
Ilya <ilya.torlin@gmail.com>
2018-08-24 15:27:57 +0500

    изменил путь для изображений, поправил стиль мильтиселекта

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = 'http://igroland-api.praweb.ru'
-export const IMAGE_URL = 'http://igroland-api.praweb.ru'
+//export const IMAGE_URL = 'http://igroland-api.praweb.ru'
+export const IMAGE_URL = ''
